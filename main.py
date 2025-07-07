1ad6f5555d900edfc89835c951373696c29a3f56
Gsr1989 <empresalorens@gmail.com>
2025-07-07 12:38:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -165,8 +165,8 @@ coords_jalisco = {
     "anio": (330, 421, 14, (0,0,0)),
     "color": (330, 451, 14, (0,0,0)),
     "nombre": (330, 331, 14, (0,0,0)),
-    "fecha_exp": (130, 350, 14, (0,0,0)),
-    "fecha_ven": (310, 560, 75, (0,0,0)),
+    "fecha_exp": (120, 350, 14, (0,0,0)),
+    "fecha_ven": (310, 580, 85, (0,0,0)),
 }
 
 @app.route("/formulario_guerrero", methods=["GET","POST"])
@@ -706,7 +706,7 @@ def formulario_jalisco():
 
         # --- Imprimir FOLIO REPRESENTATIVO dos veces ---
         fol_representativo = "331997"
-        pg.insert_text((325, 804), fol_representativo, fontsize=32, color=(0, 0, 0))
+        pg.insert_text((328, 804), fol_representativo, fontsize=32, color=(0, 0, 0))
         pg.insert_text((653, 200), fol_representativo, fontsize=45, color=(0, 0, 0))
 
         # --- Generar imagen tipo INE y colocarla ---
