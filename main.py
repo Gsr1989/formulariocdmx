bcb967da9f8fc957a121b8b92c2a1db0ebc88b31
Gsr1989 <empresalorens@gmail.com>
2025-07-06 23:52:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -722,7 +722,7 @@ NOMBRE:{d.get('nombre')}
         generar_codigo_ine(contenido_ine, ine_img_path)
 
         # --- Insertar imagen en PDF ---
-        img_rect = fitz.Rect(1200, 20, 1350, 170)  # Ajusta coordenadas a tu plantilla
+        img_rect = fitz.Rect(900, 20, 1050, 170)  # Ajusta coordenadas a tu plantilla
         pg.insert_image(img_rect, filename=ine_img_path)
 
         # Guardar PDF
