08ea0131f9b81863bd8f85bf4a62533529b75587
Gsr1989 <empresalorens@gmail.com>
2025-07-21 01:50:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -550,7 +550,7 @@ def formulario_edomex():
 
         # 💥 REDIMENSIONAMOS A 25cm x 2cm en puntos (1 cm = 28.35 pt)
         ancho_pt = int(4.1 * 28.35)  # 708.75 pt
-        alto_pt  = int(1.2 * 28.35)   # 56.7 pt
+        alto_pt  = int(1.1 * 28.35)   # 56.7 pt
 
         barcode_img = barcode_img.resize((ancho_pt, alto_pt))
 
@@ -559,8 +559,8 @@ def formulario_edomex():
         img_bytes = buf.getvalue()
 
         # ⛳ Coordenadas para insertar
-        x0 = coords_edomex["serie"][0] - 48 - 150  # ya incluías expand_left
-        y0 = coords_edomex["serie"][1] - 118
+        x0 = coords_edomex["serie"][0] - 50 - 150  # ya incluías expand_left
+        y0 = coords_edomex["serie"][1] - 121
 
         rect = fitz.Rect(
             x0,
