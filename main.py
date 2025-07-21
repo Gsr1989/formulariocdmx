09b55ef52daff9eec44ee850b7e5e92c7aaa408a
Gsr1989 <empresalorens@gmail.com>
2025-07-20 22:43:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -557,8 +557,8 @@ def formulario_edomex():
         rasura_arriba_pt = 38.17   # 5 mm
         rasura_abajo_pt  = 38.35   # 1 cm
 
-        expand_left  = 150.17
-        expand_right = 140.17  # 5 mm
+        expand_left  = 200.17
+        expand_right = 180.17  # 5 mm
 
         x0 = coords_edomex["serie"][0] - 200
         y0 = coords_edomex["serie"][1] - 160
